aa3f30ab6fd0869737feef2eef4ef00781f755ab
Ivosaja <ivoliobel@gmail.com>
2025-07-18 03:01:14 -0300

    Creation of the express's app with a simple endpoint to test and changes in the .env variables

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from "express"
+import environments from "./src/api/config/environments.js"
+
+const app = express()
+
+const PORT = environments.port
+
+app.get("/", (req, res) => {
+    res.send("Welcome to my To-Do List's API")
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port: http://localhost:${PORT}`)
+})
