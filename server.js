a58355f0ac1984d4f6cf83719681c32b72620241
Ivosaja <ivoliobel@gmail.com>
2025-07-20 19:35:11 -0300

    Adding an endpoint to create a new user and fixing an error in the database's config

--- server.js
+++ server.js
@@ -6,8 +6,41 @@ const app = express()
 
 const PORT = environments.port
 
+////////////////
+// Middlewares//
+
+app.use(express.json())
+
+
 app.get("/", (req, res) => {
     res.send("Welcome to my To-Do List's API")
+})
+
+////////////////////
+// User Endpoints //
+
+app.post("/api/user/register", async (req, res) => {
+    try{
+        const { name, email, password } = req.body
+        if(!name || !email || !password){
+            return res.status(400).json({
+                message: "Error in the request. All fields must be valid"
+            })
+        }
+        
+        const sqlQuery = `INSERT INTO usuarios (userName, userEmail, userPassword) VALUES (?, ?, ?)`
+        const [resultQuery] = await connection.query(sqlQuery, [name, email, password])
+
+        res.status(201).json({
+            message: `Creation successfully! The user with ID: ${resultQuery.insertId} was created successfully`
+        })
+
+    } catch(error){
+        console.error(error)
+        res.status(500).json({
+            message: "Internal server error creating new user in database"
+        })
+    }
 })
 
 app.listen(PORT, () => {
--- src/api/database/db.js
+++ src/api/database/db.js
@@ -1,10 +1,12 @@
 import mysql from "mysql2/promise"
-import { database } from "../config/environments.js"
+import environments from "../config/environments.js"
+
+const { database } = environments
 
 export const connection = mysql.createPool({
     host: database.host,
     database: database.name,
-    user: database.name,
+    user: database.user,
     password: database.password
 })
 
