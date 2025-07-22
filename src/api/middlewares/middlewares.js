e198357cf1acae016a493c73b332ce62bae1becd
Ivosaja <ivoliobel@gmail.com>
2025-07-21 22:06:55 -0300

    Creation of a middleware to validate the ID passed as a parameter in the URL

---
+++ src/api/middlewares/middlewares.js
@@ -0,0 +1,18 @@
+/////////////////
+// Middlewares //
+
+const validateId = (req, res, next) => {
+    const {id} = req.params
+    if(!id || isNaN(id) || parseInt(id) <= 0){
+        return res.status(400).json({
+            message: "Error in the request, the ID must be valid"
+        })
+    }
+
+    req.params.id = parseInt(id, 10)
+    next()
+}
+
+export {
+    validateId
+}
